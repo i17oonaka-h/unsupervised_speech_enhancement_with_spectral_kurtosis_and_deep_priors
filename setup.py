d82c87b99650f75946066b8883b0dd437a020a98
i17oonaka-h <ohnaka.hien@gmail.com>
2025-03-02 10:57:58 +0900

    a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="skddp_se",
     version="0.0.1",
-    install_requires=["torch", "torchaudio", "pyyaml", "tqdm", "soundfile"],
+    install_requires=["torch", "torchaudio", "pyyaml", "tqdm", "soundfile", "einops"],
     extras_require={
         "develop": ["matplotlib"]
     },
